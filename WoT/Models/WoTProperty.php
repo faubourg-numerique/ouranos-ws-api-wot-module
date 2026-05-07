455a0adb705ba511280c1ce0f210c91bd020e461
Pierre Josselin <70814177+pierre-josselin@users.noreply.github.com>
2026-05-07 11:20:37 +0200

    Update WoTProperty.php

--- WoT/Models/WoTProperty.php
+++ WoT/Models/WoTProperty.php
@@ -20,6 +20,13 @@ class WoTProperty extends Model
     public string $capacityType;
     public string $capacityValue;
     public string $hasWorkspace;
+    public ?array $enum = null;
+    public ?float $maximum = null;
+    public ?float $minimum = null;
+    public ?bool $observable = null;
+    public ?string $propertyType = null;
+    public ?bool $readonly = null;
+    public ?bool $temporal = null;
 
     public function toEntity(): Entity
     {
@@ -35,6 +42,29 @@ class WoTProperty extends Model
         $entity->setProperty("capacityType", $this->capacityType);
         $entity->setProperty("capacityValue", $this->capacityValue);
         $entity->setRelationship("hasWorkspace", $this->hasWorkspace);
+
+        if (!is_null($this->enum)) {
+            $entity->setProperty("enum", $this->enum);
+        }
+        if (!is_null($this->maximum)) {
+            $entity->setProperty("maximum", $this->maximum);
+        }
+        if (!is_null($this->minimum)) {
+            $entity->setProperty("minimum", $this->minimum);
+        }
+        if (!is_null($this->observable)) {
+            $entity->setProperty("observable", $this->observable);
+        }
+        if (!is_null($this->propertyType)) {
+            $entity->setProperty("propertyType", $this->propertyType);
+        }
+        if (!is_null($this->readonly)) {
+            $entity->setProperty("readonly", $this->readonly);
+        }
+        if (!is_null($this->temporal)) {
+            $entity->setProperty("temporal", $this->temporal);
+        }
+
         return $entity;
     }
 
@@ -50,5 +80,27 @@ class WoTProperty extends Model
         $this->capacityType = $entity->getProperty("capacityType");
         $this->capacityValue = $entity->getProperty("capacityValue");
         $this->hasWorkspace = $entity->getRelationship("hasWorkspace");
+
+        if ($entity->propertyExists("enum")) {
+            $this->enum = $entity->getProperty("enum");
+        }
+        if ($entity->propertyExists("maximum")) {
+            $this->maximum = $entity->getProperty("maximum");
+        }
+        if ($entity->propertyExists("minimum")) {
+            $this->minimum = $entity->getProperty("minimum");
+        }
+        if ($entity->propertyExists("observable")) {
+            $this->observable = $entity->getProperty("observable");
+        }
+        if ($entity->propertyExists("propertyType")) {
+            $this->propertyType = $entity->getProperty("propertyType");
+        }
+        if ($entity->propertyExists("readonly")) {
+            $this->readonly = $entity->getProperty("readonly");
+        }
+        if ($entity->propertyExists("temporal")) {
+            $this->temporal = $entity->getProperty("temporal");
+        }
     }
 }
